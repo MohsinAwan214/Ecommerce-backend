c3ef9be9a76ddddb31a5be76ae65fa518d7ad050
MohsinAwan214 <mohsinawan2267@gmai.com>
2025-01-03 15:16:29 +0500

    add changes

--- frontend/src/Component/Music.jsx
+++ frontend/src/Component/Music.jsx
@@ -5,17 +5,17 @@ import Radio from "../assets/images/Frame 694.png"
 export default function Music() {
   return (
     <div className="bg-[#000000] h-[400px] text-white flex  ">
-<div className="ml-12  h-full  max-w-96  flex flex-col border justify-between py-10 ">
-  <div className="text-[#00FF66] leading-5 ">Categories</div>
-  <div className=" text-5xl w-96  border">Enhance Your Music Experience</div>
-  <div className='flex text-black gap-5 border '>
+<div className="ml-12  h-full  max-w-96  flex flex-col  justify-between py-10 ">
+  <div className="text-[#00FF66]  ">Categories</div>
+  <div className=" text-5xl w-96  ">Enhance Your Music Experience</div>
+  <div className='flex text-black gap-5  '>
 
-   <ul className="bg-white h-12 w-12 rounded-full z-10 px-2 text-center  flex-col ">
+   <ul className="bg-white h-12 w-12 rounded-full z-10 px-2 bg-center  flex-col ">
       <li className='text-sm font-bold '>23</li>
       <li className='text-[8px] text-center '>Days</li>
    </ul>
    
-   <ul className="bg-white h-12 w-12 rounded-full z-10 px-2 text-center   ">
+   <ul className="bg-white h-12 w-12 rounded-full z-10 px-2 bg-center  flex-col  ">
       <li className='text-sm font-bold '>05</li>
       <li className='text-[8px]  text-center '>Hours</li>
    </ul>
@@ -25,13 +25,13 @@ export default function Music() {
       <li className='text-[8px]  text-center '>Minutes</li>
    </ul> 
    
-   <ul className="bg-white h-12 w-12 rounded-full z-10  text-center  ">
+   <ul className="bg-white h-12 w-12 rounded-full z-10  bg-center  flex-col  ">
       <li className='text-sm text-center font-bold text'>35</li>
       <li className='text-[8px] text-center '>Seconds</li>
    </ul>
    </div>
  <div className="bg-[#00FF66] h-14 w-[171px] BayNow-btn
-  text-white bg-center rounded border ">
+  text-white bg-center rounded  ">
 <button className="font-semibold">Bay Now!</button>
  </div>
   
